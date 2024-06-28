fdda1005fc08fcc159cb793ced9225decc054873
Carlos Henrique <127338261+chsouza1@users.noreply.github.com>
2024-06-27 21:15:13 -0300

    dd

--- src/JAVA/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.btn-mobile').addEventListener('click', function() {
-    var mobileMenu = document.getElementById('mobile_menu');
-    mobileMenu.display = mobileMenu.style.display === 'block' ? 'none' : 'block';
-});
